5e5e4abfdf04adee62d3ebd1b09eb94c8bdb4163
Ankit Anand <ankitanand@Ankits-MacBook-Pro.local>
2022-07-31 20:29:38 -0500

    Restrictes User to their page

--- django-website/Website/CCproject/views.py
+++ django-website/Website/CCproject/views.py
@@ -6,28 +6,32 @@ from .forms import CreateNewList
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
 
-    if response.method == "POST":
-        print(response.POST)
-        if response.POST.get("save"):
-            for item in ls.item_set.all():
-                if response.POST.get("c"+ str(item.id)) == "clicked":
-                    item.complete = True
+    if ls in response.user.todolist.all():
+
+        if response.method == "POST":
+            print(response.POST)
+            if response.POST.get("save"):
+                for item in ls.item_set.all():
+                    if response.POST.get("c"+ str(item.id)) == "clicked":
+                        item.complete = True
+                    else:
+                        item.complete = False
+
+                    item.save()
+
+            elif response.POST.get("newItem"):
+                txt = response.POST.get("new")
+                if len(txt) > 2:
+                    ls.item_set.create(text=txt, complete=False)
                 else:
-                    item.complete = False
+                    print("invalid")
 
-                item.save()
+        #item = ls.item_set.get(id=id)
+        #return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, str(item.text)))
 
-        elif response.POST.get("newItem"):
-            txt = response.POST.get("new")
-            if len(txt) > 2:
-                ls.item_set.create(text=txt, complete=False)
-            else:
-                print("invalid")
+        return render(response, "CCproject/list.html", {"ls":ls})
 
-    #item = ls.item_set.get(id=id)
-    #return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, str(item.text)))
-
-    return render(response, "CCproject/list.html", {"ls":ls})
+    return render(response, "CCproject/view.html", {})
 
 def home(response):
     return render(response, "CCproject/home.html", {})
@@ -37,11 +41,18 @@ def create(response):
         form = CreateNewList(response.POST)
 
         if form.is_valid():
+
             n = form.cleaned_data["name"]
             t = ToDoList(name=n)
             t.save()
+            response.user.todolist.add(t)
 
             return HttpResponseRedirect("/%i" %t.id)
     else:
         form = CreateNewList()
+
     return render(response, "CCproject/create.html", {"form":form})
+
+def view(response):
+
+    return render(response, "CCproject/view.html", {})
--- django-website/Website/Website/settings.py
+++ django-website/Website/Website/settings.py
@@ -127,5 +127,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
-LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "/create"
+LOGOUT_REDIRECT_URL = "/home"
